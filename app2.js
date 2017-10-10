cc5e9c4b597ffeb72ef1eaa4d37225fc657b4a9a
pilarvm <32286800+pilarvm@users.noreply.github.com>
2017-10-10 12:54:27 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,6 +1,7 @@
 var isValidCard = function(number){
     var result='No es tarjeta valida';
     var perTwo=1;
+    var sum=0;
 
     if(number.length==16){
         var array = number.split("");
@@ -10,16 +11,23 @@ var isValidCard = function(number){
         for(i=0; i<=array.length; i++){
             if (i%2!=0){
               perTwo=array[i]*2;
+              if(perTwo>9){
+                var twoDigits = (perTwo).toString(); 
+                perTwo = parseInt(twoDigits[0]) + parseInt(twoDigits[1]); 
+
+              }
               newArray.push(perTwo);
-              
+              sum += perTwo;
             }
           
     }
 
-
+if(sum % 10 === 0){
     result= 'tarjeta valida :)'    
     }
-
+    }
+console.log(newArray)
+console.log(sum)
     return alert(result);
 }
 
